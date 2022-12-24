ae849ca3d7939cf0590a1ddf2f4a5266eeb3eaf2
priyam <priyamshankar.5@gmail.com>
2022-12-24 21:54:08 +0530

    adding the driver home blank page and installing the browserRouter

---
+++ vitds/src/pages/DriverHome.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DriverHome = () => {
+  return (
+  <>
+    <div>DriverHome</div>
+    <h1>this is the driver home page for the react router dom.</h1>
+  </>
+  )
+}
+
+export default DriverHome
