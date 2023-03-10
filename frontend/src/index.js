2702e8d637cfb324c4dffbcf866f60b8c6371dff
Kumar Shanu <kumar.shanu2020@vitbhopal.ac.in>
2023-03-11 00:52:14 +0530

    Added a counter component to test the redux store, works fine

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,15 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // import './index.css';
 import App from "./App";
-// import { Provider } from "react-redux";
+import { Provider } from "react-redux";
+import { store } from './redux/store'
 // import { store } from "./state/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
+    <Provider store={store}>
       <App />
-    {/* </Provider> */}
+    </Provider>
   </React.StrictMode>
 );
 
